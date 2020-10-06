c1ca6ef58e1909424f33c03017c0689e444ec98e
davidwalser <walser.dave@gmail.com>
2020-10-06 19:40:28 +0200

    First instance pool

---
+++ InstancePool.tf
@@ -0,0 +1,29 @@
+variable "zone" {
+  default = "at-vie-1"
+}
+
+data "exoscale_compute_template" "ubuntu" {
+  zone = var.zone
+  name = "Linux Ubuntu 20.04 LTS 64-bit"
+}
+
+resource "exoscale_instance_pool" "instancepool" {
+  name         = "instancepool"
+  zone         = var.zone
+  description = "david_instancepool"
+  template_id  = data.exoscale_compute_template.ubuntu.id
+  service_offering = "micro"
+  size         = 2
+  disk_size    = 10
+  key_pair     = ""
+
+  #affinity_groups = []
+  #security_groups = [exoscale_security_group.sg.name]
+
+  user_data = <<EOF
+#!/bin/bash
+set -e
+apt update
+apt install -y nginx
+EOF
+}
