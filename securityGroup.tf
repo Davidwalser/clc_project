d3829fa63b7a664b070aa7ec0715798ad1af0f38
davidwalser <walser.dave@gmail.com>
2020-10-10 15:26:28 +0200

    Renamed resource group and added two new rules

--- securityGroup.tf
+++ securityGroup.tf
@@ -1,5 +1,5 @@
 resource "exoscale_security_group" "sg" {
-  name = "exercise-2"
+  name = "security_group"
 }
 resource "exoscale_security_group_rule" "http" {
   security_group_id = exoscale_security_group.sg.id
@@ -8,4 +8,22 @@ resource "exoscale_security_group_rule" "http" {
   cidr = "0.0.0.0/0"
   start_port = 80
   end_port = 80
+}
+
+resource "exoscale_security_group_rule" "nlb" {
+  security_group_id = exoscale_security_group.sg.id
+  type = "INGRESS"
+  protocol = "tcp"
+  cidr = "0.0.0.0/0"
+  start_port = 8080
+  end_port = 8080
+}
+
+resource "exoscale_security_group_rule" "ssh" {
+  security_group_id = exoscale_security_group.sg.id
+  type = "INGRESS"
+  protocol = "tcp"
+  cidr = "0.0.0.0/0"
+  start_port = 22
+  end_port = 22
 }
