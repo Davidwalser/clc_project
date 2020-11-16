cf0f693f7fba67fe2e63c8759258b28f4249aa64
davidwalser <walser.dave@gmail.com>
2020-11-16 17:06:16 +0100

    Now working with lectures SD

--- UserData/prometheus.sh
+++ UserData/prometheus.sh
@@ -22,32 +22,11 @@ EOCF
 
 docker volume create --name SharedDir
 
-# sadly didnt work, no idea why
-# docker run \
-#       -d \
-#       -v SharedDir:/srv/service-discovery/ \
-#       -e EXOSCALE_KEY=${exoscale_key} \
-#       -e EXOSCALE_SECRET=${exoscale_secret} \ 
-#       -e EXOSCALE_ZONE_ID="4da1b188-dcd6-4ff5-b7fd-bde984055548" \
-#       -e EXOSCALE_INSTANCEPOOL_ID=${exoscale_instancepool_id} \
-#       -e TARGET_PORT="9100" \
-#       -- name pythonservice \
-#       davidwalser/servicediscovery
-
 docker run \
     -d \
     -p 9090:9090 \
-    -v SharedDir:/srv/service-discovery/ \
+    -v SharedDir:/srv/service-discovery \
     -v /srv/prometheus.yml:/etc/prometheus/prometheus.yml \
     prom/prometheus
 
-# using the service of a friend (NOT MINE!)
-docker run \
-    -d \
-    -v SharedDir:/srv/service-discovery/ \
-    -e EXOSCALE_KEY=${exoscale_key} \
-    -e EXOSCALE_SECRET=${exoscale_secret} \
-    -e EXOSCALE_ZONE=${exoscale_zone} \
-    -e EXOSCALE_INSTANCEPOOL_ID=${exoscale_instancepool_id} \
-    -e TARGET_PORT=9100 \
-    deitsch/exoscale_sd
+docker run -d -v SharedDir:/var/run/prometheus-sd-exoscale-instance-pools janoszen/prometheus-sd-exoscale-instance-pools --exoscale-api-key ${exoscale_key} --exoscale-api-secret ${exoscale_secret} --exoscale-zone-id 4da1b188-dcd6-4ff5-b7fd-bde984055548 --instance-pool-id ${exoscale_instancepool_id} --prometheus-port 9100
