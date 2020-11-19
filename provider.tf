46fc81f8c0f9cba856e8b1b483302158b52475d3
davidwalser <walser.dave@gmail.com>
2020-11-19 17:53:28 +0100

    removed unused zone_id variable

--- provider.tf
+++ provider.tf
@@ -17,10 +17,6 @@ variable "zone" {
   type = string
   default = "at-vie-1"
 }
-variable "zone_id" {
-  type = string
-  default = "4da1b188-dcd6-4ff5-b7fd-bde984055548"
-}
 provider "exoscale" {
   key = var.exoscale_key
   secret = var.exoscale_secret
